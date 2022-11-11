63cf1bb4e6d3136383f72acbae0e8868fa277c0a
Debargha-Mitra-Roy <developer.debarghamitraroy@gmail.com>
2022-11-11 20:43:24 +0530

    Fourty-Sixth Commit

---
+++ Primes sum.cpp
@@ -0,0 +1,52 @@
+/*
+    Primality Test :-
+
+    Link:   https://practice.geeksforgeeks.org/batch-problems/primality-test/0/
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Solution
+{
+private:
+    bool isPrime(int N)
+    {
+        for (int i = 2; i <= sqrt(N); i++)
+        {
+            if (N % i == 0)
+                return 0;
+        }
+
+        return 1;
+    }
+
+public:
+    string isSumOfTwo(int N)
+    {
+        for (int i = 2; i < N; i++)
+        {
+            if (isPrime(N - i) && isPrime(i))
+                return "Yes";
+        }
+
+        return "No";
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        cout << ob.isSumOfTwo(N) << "\n";
+    }
+
+    return 0;
+}
